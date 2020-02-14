0d7c87cf14dd5eb394df0becb5e2fdd9831f99e0
adhip <adhip@arista.com>
2020-02-14 13:49:21 -0800

    cloudeos/terraform: Add Sonu's fix to cloudeos repo
    Also fixed the linker issue for the examples
    
    Change-Id: Ia8fc86a288cf49bdb7f675468cce8dfd2e9d4029

--- terraform/module/arista/aws/subnet/output.tf
+++ terraform/module/arista/aws/subnet/output.tf
@@ -1,6 +1,6 @@
 output "vpc_subnets" {
   description = "The ids of subnets created inside the new vpc"
-  value       = aws_subnet.subnet.*.id
+  value       = arista_subnet.subnet.*.computed_subnet_id
 }
 output "subnet_cidr" {
   description = "map of subnet cidr to id"
