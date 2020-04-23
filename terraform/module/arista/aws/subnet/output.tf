b4f0ee79f22d7c342f85c5b4c946b63c502d31b5
adhip <adhip@arista.com>
2020-04-22 17:37:12 -0700

    cloudeos/module: Fix zipmap/keys issue for subnets
    Most of the times zipmap complains that either there is a mismatch
    or the list is empty. Creating an list of nil strings which won't
    cause access errors.
    
    Change-Id: I50a09d66a5549facc675289b4b4dc5b198fc2e96

--- terraform/module/arista/aws/subnet/output.tf
+++ terraform/module/arista/aws/subnet/output.tf
@@ -1,11 +1,11 @@
 // Copyright (c) 2020 Arista Networks, Inc.
 // Use of this source code is governed by the Apache License 2.0
 // that can be found in the LICENSE file.
+locals {
+  empty_subnet_id    = [for name in var.subnet_names : ""]
+  computed_subnet_id = length(arista_subnet.subnet.*.computed_subnet_id) > 0 ? arista_subnet.subnet.*.computed_subnet_id : local.empty_subnet_id
+}
 output "vpc_subnets" {
   description = "The ids of subnets created inside the new vpc"
-  value       = arista_subnet.subnet.*.computed_subnet_id
-}
-output "subnet_cidr" {
-  description = "map of subnet cidr to id"
-  value       = length(aws_subnet.subnet.*.id) > 0 ? zipmap(keys(var.subnet_names), aws_subnet.subnet.*.id) : {}
+  value       = local.computed_subnet_id
 }
