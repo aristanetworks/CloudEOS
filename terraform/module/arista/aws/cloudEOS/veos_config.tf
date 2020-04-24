a268ef1989a7adc7f1f2681581bf41c1220878fb
adhip <adhip@arista.com>
2020-04-24 12:02:25 -0700

    cloudeos/module: Adding ForceNew attributes in cloudeos
    https://gerrit.corp.arista.io/c/arista/+/111360/ made provider
    and module changes. Porting the module changes in CloudEOS.
    
    Change-Id: I6b46507293d6151ea2350c73bffcf941f4059152

--- terraform/module/arista/aws/cloudEOS/veos_config.tf
+++ terraform/module/arista/aws/cloudEOS/veos_config.tf
@@ -6,16 +6,19 @@ locals {
 }
 
 resource "arista_veos_config" "veos" {
-  count          = length(values(var.subnetids)) != 0 && var.topology_name != "" ? 1 : 0
-  cloud_provider = "aws"
-  topology_name  = var.topology_name // ListTopology will get us clos_name, wan_name
-  role           = var.role
-  cnps           = lookup(var.tags, "Cnps", "")
-  vpc_id         = var.vpc_info[0][0] //ListVpc will get us the router role and region
-  tags           = var.tags           //tags should have the router name
-  region         = var.region
-  is_rr          = var.is_rr
-  intf_name      = var.intf_names
+  count             = length(values(var.subnetids)) != 0 && var.topology_name != "" ? 1 : 0
+  cloud_provider    = "aws"
+  topology_name     = var.topology_name // ListTopology will get us clos_name, wan_name
+  role              = var.role
+  cnps              = lookup(var.tags, "Cnps", "")
+  vpc_id            = var.vpc_info[0][0] //ListVpc will get us the router role and region
+  tags              = var.tags           //tags should have the router name
+  region            = var.region
+  is_rr             = var.is_rr
+  ami               = var.cloudeos_ami
+  key_name          = var.keypair_name
+  availability_zone = var.availability_zone
+  intf_name         = var.intf_names
   //Only sending the first IP address of the list.
   intf_private_ip = local.private_ip
   intf_type       = values(var.interface_types)
