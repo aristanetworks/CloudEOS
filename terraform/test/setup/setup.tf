58c7cd6cb1a4cc68a6d8f9aa2a6ae4e086e30174
Rahul B S <rahulbs@arista.com>
2020-05-08 07:35:57 -0700

    CloudEOS/terraform: Set version for aws provider
    Change-Id: I52ebd610755e70726ddd0cd4612e27d80c80afd2

--- terraform/test/setup/setup.tf
+++ terraform/test/setup/setup.tf
@@ -1,5 +1,7 @@
 ## do not remove this file..used for downloading terraform plugins during setup
-provider "aws" {}
+provider "aws" {
+  version = "=2.50"
+}
 provider "azurerm" {
   skip_provider_registration = true
   version                    = 1.33
