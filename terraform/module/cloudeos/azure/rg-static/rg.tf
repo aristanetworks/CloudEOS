37dcc9600a43a8753d9acf8ce5b645e7755ac464
manishp <manishp@arista.com>
2022-04-11 02:16:27 -0700

    terraform/module: Refactor security rule from security group
    Azure security group definition allows in-line security rule as well as
    standalone security rules but not both. This change creates standalone
    security rules so that one can add security rules later via scripts.
    
    closes=BUG681462
    
    Change-Id: Ie2b315aeebee54fb2a28232d0fc109f846a12d8c
    Signed-off-by: manishp <manishp@arista.com>

--- terraform/module/cloudeos/azure/rg-static/rg.tf
+++ terraform/module/cloudeos/azure/rg-static/rg.tf
@@ -6,44 +6,54 @@ resource "azurerm_network_security_group" "publicNSG" {
   name                = var.nsg_name
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = var.rg_name
-
-  security_rule {
-    name                       = "allow_SSH"
-    description                = "Allow SSH access"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-  security_rule {
-    name                       = "allow_IKE500"
-    description                = "Allow IKE access"
-    priority                   = 110
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Udp"
-    source_port_range          = "*"
-    destination_port_range     = "500"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-  security_rule {
-    name                       = "allow_IKE4500"
-    description                = "Allow IKE4500 access"
-    priority                   = 120
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Udp"
-    source_port_range          = "*"
-    destination_port_range     = "4500"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
 }
+
+resource "azurerm_network_security_rule" "publicSSH" {
+  count                      = var.role == "CloudEdge" ? 1 : 0
+  name                       = "allow_SSH"
+  description                = "Allow SSH access"
+  priority                   = 100
+  direction                  = "Inbound"
+  access                     = "Allow"
+  protocol                   = "Tcp"
+  source_port_range          = "*"
+  destination_port_range     = "22"
+  source_address_prefix      = "*"
+  destination_address_prefix = "*"
+  resource_group_name         = var.rg_name
+  network_security_group_name = azurerm_network_security_group.publicNSG[count.index].name
+}
+resource "azurerm_network_security_rule" "publicIKE500" {
+  count                      = var.role == "CloudEdge" ? 1 : 0
+  name                       = "allow_IKE500"
+  description                = "Allow IKE access"
+  priority                   = 110
+  direction                  = "Inbound"
+  access                     = "Allow"
+  protocol                   = "Udp"
+  source_port_range          = "*"
+  destination_port_range     = "500"
+  source_address_prefix      = "*"
+  destination_address_prefix = "*"
+  resource_group_name         = var.rg_name
+  network_security_group_name = azurerm_network_security_group.publicNSG[count.index].name
+}
+resource "azurerm_network_security_rule" "publicIKE4500" {
+  count                      = var.role == "CloudEdge" ? 1 : 0
+  name                       = "allow_IKE4500"
+  description                = "Allow IKE4500 access"
+  priority                   = 120
+  direction                  = "Inbound"
+  access                     = "Allow"
+  protocol                   = "Udp"
+  source_port_range          = "*"
+  destination_port_range     = "4500"
+  source_address_prefix      = "*"
+  destination_address_prefix = "*"
+  resource_group_name         = var.rg_name
+  network_security_group_name = azurerm_network_security_group.publicNSG[count.index].name
+}
+
 
 resource "azurerm_network_security_group" "privateNSG" {
   count               = var.role != "CloudEdge" ? 1 : 0
@@ -51,18 +61,20 @@ resource "azurerm_network_security_group" "privateNSG" {
   name                = "${var.nsg_name}-leaf"
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = var.rg_name
-
-  security_rule {
-    name                       = "allow_all"
-    priority                   = 130
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "*"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
+}
+resource "azurerm_network_security_rule" "privateAll" {
+  count                      = var.role != "CloudEdge" ? 1 : 0
+  name                       = "allow_all"
+  priority                   = 130
+  direction                  = "Inbound"
+  access                     = "Allow"
+  protocol                   = "*"
+  source_port_range          = "*"
+  destination_port_range     = "*"
+  source_address_prefix      = "*"
+  destination_address_prefix = "*"
+  resource_group_name         = var.rg_name
+  network_security_group_name = azurerm_network_security_group.privateNSG[count.index].name
 }
 
 data "azurerm_resource_group" "rg" {
